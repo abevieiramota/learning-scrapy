4b11dce0052825f678236c44d024579ce4ca2b51
Abelardo Vieira Mota <abevieiramota@gmail.com>
2015-07-10 23:25:18 -0300

    adicionado scrapy do lolhehehe e inicializado o do dotabuff

---
+++ lolhehehe/lolhehehe/spiders/LolheheheSpider.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from lolhehehe.items import LolheheheItem
+
+
+class LolhehehespiderSpider(scrapy.Spider):
+
+    name = "LolheheheSpider"
+    start_urls = (
+        'http://lolhehehe.com/category/galeria',
+    )
+
+    def parse(self, response):
+
+        for galeria_url in response.xpath('//p/a[contains(@class, "more-link")]/@href').extract():
+
+            yield scrapy.Request(galeria_url, callback=self.parse_galeria)
+
+        for page_url in response.xpath(u'//a[contains(text(), "Próxima página")]/@href').extract():
+
+            yield scrapy.Request(page_url)
+
+
+    def parse_galeria(self, response):
+
+        img_urls = response.xpath('//p/img/@src').extract()
+
+        item = LolheheheItem()
+        item['file_urls'] = img_urls
+
+        yield item
